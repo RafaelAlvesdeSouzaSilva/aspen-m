fe556cd72c397dc2efb04ff84c952d9b5a44424a
RafaelAlvesdeSouzaSilva <rafinhaalvesss70@gmail.com>
2026-06-15 18:40:28 -0300

    fix: corrige caminhos das imagens e newArchEnabled

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://192.168.15.10:3000';
+const BASE_URL = 'https://aspen-api-crqt.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
